ddbab2c53c1f1fb2a3a8d881feee0d8a85a2841c
Oladeji Victoria <nursedeji@gmail.com>
2025-07-29 22:35:34 +0100

    first commit

---
+++ Intro.py
@@ -0,0 +1,43 @@
+# #PLP = "Academy"
+# Student_Name ="Abdi Kadir"
+# Student_ID = 1001
+# Student_Age = 20.2
+# Fees_clearance = True
+
+# #output
+# print(Student_Name, Student_ID, Student_Age, Fees_clearance)
+
+""""#Lists
+languages = ["Python", "Java", "C++", "JavaScript"]
+print(languages)
+
+Shopping_list = ["Milk", "Eggs", "Bread", "Butter"]
+print(Shopping_list)
+
+#Tuples
+products = ('XBox', 499.99, "Habibi", 23)
+print(products)"""
+
+""""#Sets
+Set_A = {1, 2, 3, 4, 5}
+Set_B = {4, 5, 6, 7, 8}
+print(Set_A)
+print(Set_B)"""
+
+
+#Dictionaries
+student_info = {
+    "Name": "Abdi Kadir",
+    "ID": 1001,
+    "Age": 20.2,
+    "Fees Clearance": True }
+print(student_info)
+
+#Logical Operators
+a = 10
+b = 20
+c = 30
+print(a > b and b < c)  # True 
+print(a < b or b > c)   # True
+print(not (a < b))       # False    
+ 
